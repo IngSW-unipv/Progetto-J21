bffcea29650b3626efc71ed0957b9f3a1009c13e
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-04-26 17:28:49 +0200

    Classe di prova

---
+++ src/it/unipv/ingsw/electronicticketingsystem/Prova.java
@@ -0,0 +1,11 @@
+package it.unipv.ingsw.electronicticketingsystem;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 8;
+		System.out.println(x);
+	}
+
+}
