498a01d32fdadf14b2079d75b3c1fa778deb0f4d
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-04-28 11:38:37 +0200

    Sistemazione dei package

---
+++ src/it/unipv/ingsw/electronicticketingsystem/util/sale/Sale.java
@@ -0,0 +1,36 @@
+package it.unipv.ingsw.electronicticketingsystem.util.sale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sale {
+	private boolean completed;
+	private List<SaleLineItem> items;
+	private int quantity; // provvisorio
+	private double total; //meglio se viene recuperato da TicketDescription 
+	
+	public Sale() {
+		this.completed=false; //posizione aperta appena viene invocato
+		this.items=new ArrayList<>();
+		this.quantity=0;
+	}
+	
+	public void addSale(double tipo) {
+		//double tipo da cambiare, sostituire con 'TicketType t' 
+		SaleLineItem s=new SaleLineItem(tipo); 
+		items.add(s);
+		this.quantity++; 
+	}
+	
+	public void setCompleted() {
+		this.completed=true;
+	}
+	
+	public double getTotal() {
+		double t=0;
+		for(SaleLineItem i: items) {
+			t=+i.getSubTotal();			
+		}
+		return t;
+	}
+}
---
+++ src/it/unipv/ingsw/electronicticketingsystem/util/sale/SaleLineItem.java
@@ -0,0 +1,22 @@
+package it.unipv.ingsw.electronicticketingsystem.util.sale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaleLineItem {
+	private double price;
+
+	
+	public SaleLineItem(double p) {
+		this.price=p; 
+		//un'alternativa può essere il coinvolgimento del catalogo specificando 
+		//solo il tipo del biglietto in questo costruttore.
+		//idealmente dovremmo avere un metodo che ci permette di avere il dato desiderato specificando solo il tipo
+	}
+	
+	public double getSubTotal() {
+		return price;
+	}
+	
+
+}
