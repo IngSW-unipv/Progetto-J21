196a08afb2d12a401b4b58b0fa5c50c7dbc372be
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-05-03 13:47:25 +0200

    Aggiunta generatore di id e relativo test

--- src/it/unipv/ingsw/electronicticketingsystem/test/TicketTester.java
+++ src/it/unipv/ingsw/electronicticketingsystem/test/TicketTester.java
@@ -8,8 +8,20 @@ public class TicketTester {
 		// TODO Auto-generated method stub
 		TicketCatalog catalog = new TicketCatalog();
 		
+		// Test della corretta stampa del catalogo
 		catalog.printTicketCatalog();
 		
+		
+		// Test della corretta generazione dei TicketID
+		TravelDocument td1 = new Carnet();
+		TravelDocument td2 = new SingleRideTicket();
+		TravelDocument td3 = new SingleRideTicket();
+		TravelDocument td4 = new BusPass();
+		
+		System.out.println(td1.generateTicketID());
+		System.out.println(td2.generateTicketID());
+		System.out.println(td3.generateTicketID());
+		System.out.println(td4.generateTicketID());
 		
 	}
 
--- src/it/unipv/ingsw/electronicticketingsystem/util/ticket/TravelDocument.java
+++ src/it/unipv/ingsw/electronicticketingsystem/util/ticket/TravelDocument.java
@@ -1,15 +1,17 @@
 package it.unipv.ingsw.electronicticketingsystem.util.ticket;
 
 public abstract class TravelDocument {
-
+	
 	private int type;
 	private String description;
 	private double price;
+	private String TicketID;
 	
 	public TravelDocument(int type, String description, double price) {
 		this.type = type;
 		this.description = description;
 		this.price = price;
+		this.TicketID = generateTicketID();
 	}
 
 	public int getType() {
@@ -24,5 +26,15 @@ public abstract class TravelDocument {
 		return price;
 	}
 	
+	public String getTicketID() {
+		return TicketID;
+	}
+	
+	public String generateTicketID() {
+		String typeCode = Integer.toString(getType());
+		String descriptionCode = getDescription().substring(0, 3).toUpperCase();
+		String randomCode = Integer.toString((int)(Math.random()*10000));
+		return typeCode + descriptionCode + randomCode;
+	}
 	
 }
