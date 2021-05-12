0d7c456e708190f3fdb1bf884ae2b0a860095c6a
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-05-12 15:49:33 +0200

    Spostamento metodo per generazione ticketID

--- src/it/unipv/ingsw/electronicticketingsystem/util/sale/SaleLineItem.java
+++ src/it/unipv/ingsw/electronicticketingsystem/util/sale/SaleLineItem.java
@@ -6,13 +6,25 @@ import it.unipv.ingsw.electronicticketingsystem.util.ticket.*;
 public class SaleLineItem {
 
 	private TravelDocument ticket; //ticket contiene tutti i dati utili per questa linea
+	private String ticketID;
 	
 	public SaleLineItem(int type) {
-		this.ticket=TicketCatalog.getSelectedTravelDocument(type); 
+		this.ticket=TicketCatalog.getSelectedTravelDocument(type);
+		this.ticketID = generateTicketID();
 	}
 	
 	public double getSubTotal() {
 		return ticket.getPrice();
 	}
+	
+	public String generateTicketID() {
+		String typeCode = Integer.toString(ticket.getType());
+		String descriptionCode = ticket.getDescription().substring(0, 3).toUpperCase();
+		String randomCode = Integer.toString((int)(Math.random()*10000));
+		return typeCode + descriptionCode + randomCode;
+	}
 
+	public String getTicketID() {
+		return ticketID;
+	}
 }
--- src/it/unipv/ingsw/electronicticketingsystem/util/ticket/TravelDocument.java
+++ src/it/unipv/ingsw/electronicticketingsystem/util/ticket/TravelDocument.java
@@ -5,15 +5,11 @@ public abstract class TravelDocument {
 	private int type;
 	private String description;
 	private double price;
-	private String TicketID;
-	private boolean validated;
 	
 	public TravelDocument(int type, String description, double price) {
 		this.type = type;
 		this.description = description;
 		this.price = price;
-		this.TicketID = generateTicketID();
-		this.validated = false;
 	}
 
 	public int getType() {
@@ -28,19 +24,5 @@ public abstract class TravelDocument {
 		return price;
 	}
 	
-	public String getTicketID() {
-		return TicketID;
-	}
-	
-	public void validateTravelDocument() {
-		this.validated = true;
-	}
-	
-	public String generateTicketID() {
-		String typeCode = Integer.toString(getType());
-		String descriptionCode = getDescription().substring(0, 3).toUpperCase();
-		String randomCode = Integer.toString((int)(Math.random()*10000));
-		return typeCode + descriptionCode + randomCode;
-	}
 	
 }
