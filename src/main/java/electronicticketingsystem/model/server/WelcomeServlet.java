6b2071688addbbb014ddb184c06c76c1daa875a7
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-06-23 16:27:18 +0200

    Modifiche collegamenti a pagina cart

--- src/main/java/electronicticketingsystem/model/server/WelcomeServlet.java
+++ src/main/java/electronicticketingsystem/model/server/WelcomeServlet.java
@@ -14,25 +14,40 @@ import electronicticketingsystem.model.util.ticket.BusPass;
 import electronicticketingsystem.model.util.ticket.Carnet;
 import electronicticketingsystem.model.util.ticket.SingleRideTicket;
 import electronicticketingsystem.model.util.ticket.TravelDocument;
+import electronicticketingsystem.model.util.sale.Sale;
 import electronicticketingsystem.model.util.ticket.*;
 
 public class WelcomeServlet extends HttpServlet{
 	
-	
+	TicketMachine tm = new TicketMachine();
+	Sale s = tm.makeSale();
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		requests(req,resp);
-	} 
+		//requests(req,resp);
+		if(req.getPathInfo().equals("/")) {
+			home(req,resp);}
+		if(req.getPathInfo().equals("/purchase") )
+			purchase(req,resp);
+		if(req.getPathInfo().equals("/cart")) {
+			resp.getWriter().write(Rythm.render("cart.html"));}
+	}
 	
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		requests(req,resp);
+		//requests(req,resp);
+		if(req.getPathInfo().equals("/")) {
+			home(req,resp);}
+		if(req.getPathInfo().equals("/purchase") )
+			purchase(req,resp);
+		if(req.getPathInfo().equals("/cart")) {
+			resp.getWriter().write(Rythm.render("cart.html",s));}
+	
 	}
 	
 	protected void requests(HttpServletRequest req,HttpServletResponse resp)throws ServletException, IOException{
 		if(req.getPathInfo().equals("/")) {
 			home(req,resp);
-		}if(req.getPathInfo().equals("/purchase") )
+		} else if(req.getPathInfo().equals("/purchase") )
 			purchase(req,resp);
 		}
 	
@@ -45,20 +60,26 @@ public class WelcomeServlet extends HttpServlet{
 		
 	protected void purchase(HttpServletRequest req,HttpServletResponse resp)throws ServletException, IOException{
 		resp.getWriter().write(Rythm.render("purchase.html"));
-		if(req.getPathInfo().equals("/save")) {
+		if(req.getPathInfo().equals("purchase/cart")) {
 			shop(req,resp);
-		} else if(req.getPathInfo().equals("/cart")) {
-			resp.getWriter().write(Rythm.render("cart.html"));
-		}
-		
-				
+		} 	
 	}
 	
-	protected void shop(HttpServletRequest req,HttpServletResponse resp)throws ServletException, IOException{
-		TicketMachine tm = new TicketMachine();
-		tm.makeSale();
+	protected void shop(HttpServletRequest req,HttpServletResponse resp)throws ServletException, IOException {
+		int type = Integer.parseInt(req.getParameter("type"));
+		int qty = Integer.parseInt(req.getParameter("qty"));
 		
-		//tm.enterItem(req.getParameter("id"), req.getParameter("qty"));
+		TicketMachine tm = new TicketMachine();
+		Sale s = tm.makeSale();
+		tm.enterItem(type, qty);
+		resp.getWriter().write(Rythm.render("cart.html", s));
+		
+		if(req.getPathInfo().equals("/purchase")) {
+			purchase(req,resp);
+		} 	
+		
+		
+		
 	}
 
 }
