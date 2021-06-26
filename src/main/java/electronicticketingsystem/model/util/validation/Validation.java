f6392b0b38e352dec8afa58a33ba3b0878364c3a
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-06-26 12:27:40 +0200

    Sistemazione commenti package validation per javadoc

--- src/main/java/electronicticketingsystem/model/util/validation/Validation.java
+++ src/main/java/electronicticketingsystem/model/util/validation/Validation.java
@@ -4,17 +4,31 @@ import java.time.LocalTime;
 import electronicticketingsystem.model.util.sale.SaleLineItem;
 import electronicticketingsystem.model.util.sale.SoldRegister;
 
+/**
+ * Classe che descrive una convalida. Una convalida è descritta dai seguenti attributi:
+ * @param expirationTime		valore di tipo LocalTime che indica l'orario di validità del biglietto
+ * @param id					valore di tipo String che indica l'id del biglietto a cui è associata la convalida
+ *
+ */
 public class Validation {
 	private LocalTime expirationTime;
 	private String id;
 
-	
+	/**
+	 * Costruttore della classe, che crea un oggetto di tipo Validation a partire dall'id del biglietto da convalidare.
+	 * @param id				id del biglietto da convalidare
+	 */
 	public Validation(String id) {
 		this.id = id;
 		setExpirationTime();
 		SoldRegister.returnTicket(id).setOneAccessLess(); //quando viene effettuata una convalida si abbassa di uno il contatore delle corse
 	}
 	
+	/**
+	 * Metodo per il calcolo dell'orario di scadenza del biglietto tramite la classe LocalTime del package java.time
+	 * Il metodo aggiunge le ore di validità previste dal tipo di biglietto all'orario di convalida (cioè quello 
+	 * corrente).
+	 */
 	private void setExpirationTime() {
 		this.expirationTime=LocalTime.now(); //Obtains the current time from the system clock in the default time-zone.
 		SaleLineItem ticket=SoldRegister.returnTicket(id);
@@ -22,10 +36,18 @@ public class Validation {
 		
 	}
 	
+	/**
+	 * Metodo get per risalire all'orario di scadenza del biglietto 
+	 * @return expirationTime (LocalTime)
+	 */
 	public LocalTime getExpirationTime() {
 		return this.expirationTime;
 	}
 	
+	/**
+	 * Metodo get per risalire all'id del biglietto associato alla convalida
+	 * @return id (String)
+	 */
 	public String getID() {
 		return this.id;
 	}
--- src/main/java/electronicticketingsystem/model/util/validation/ValidationRegister.java
+++ src/main/java/electronicticketingsystem/model/util/validation/ValidationRegister.java
@@ -3,14 +3,30 @@ package electronicticketingsystem.model.util.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe che descrive il registro delle convalide.
+ * Questa classe è realizzata seguendo il design pattern Singleton della GoF in quanto ogni emettitrice
+ * deve possedere un'unica istanza del registro che contiene tutti i biglietti convalidati.
+ * @param stampedTickets		è una lista di oggetti di tipo Validation
+ * 
+ */
 public class ValidationRegister {
 	private static List<Validation> stampedTickets;
 	private static ValidationRegister instance = null;
 	
+	/**
+	 * Costruttore della classe che definisce il registro come ArrayList
+	 */
 	public ValidationRegister() {
 		ValidationRegister.stampedTickets=new ArrayList<>();
 	}
 	
+	/**
+	 * Metodo statico previsto dal pattern Singleton. Dovendo esistere un'unica istanza di questa classe,
+	 * il metodo ne crea una e la restituisce nel caso non esista, oppure restituisce quella esistente 
+	 * nel caso in cui ci sia già un'istanza
+	 * @return ValidationRegister		restituisce l'unica istanza esistente della classe stessa
+	 */
 	public static ValidationRegister getInstance() {
 		if (instance == null) {
 			instance = new ValidationRegister();
@@ -18,10 +34,20 @@ public class ValidationRegister {
 		return instance;
 	}
 	
+	
+	/**
+	 * Metodo che aggiunge una certa convalida al registro
+	 * @param v							richiede in ingresso l'oggetto della classe Validation da aggiungere
+	 */
 	public void addToRegister(Validation v) {
 		stampedTickets.add(v); //aggiungi alla lista tutte le convalide	
 	}
 	
+	/**
+	 * Metodo che restituisce la convalida corrispondente all'id in ingresso
+	 * @param id						l'ID del biglietto del quale si vuole ottenere la convalida
+	 * @return Validation				restituisce un oggetto di tipo Validation corrispondente all'id inserito
+	 */
 	public static Validation findValidation(String id) {
 		for(Validation i: stampedTickets) {
 			if(i.getID().equals(id)) return i;
@@ -29,10 +55,13 @@ public class ValidationRegister {
 
 	}
 	
+	/**
+	 * Metodo di stampa del catalogo perchè possa essere visualizzato a schermo come ID + orario di convalida
+	 */
 	public void printValidationRegister() {
 		System.out.println("Validated tickets' register:");
 		for (Validation i : stampedTickets) {
-			System.out.println("ID: "+i.getID() + " Expiration Time: " + i.getExpirationTime());
+			System.out.println("ID: "+i.getID() + " Expiration time: " + i.getExpirationTime());
 		}
 	}
 	
