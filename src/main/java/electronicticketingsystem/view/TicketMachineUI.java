a2b124571b042c4a14d3f2d325a67fa7146b9cfa
CamillaCP <80338467+CamillaCP@users.noreply.github.com>
2021-06-29 11:57:31 +0200

    Aggiunta pagamento con carta di credito e commenti per javadoc

--- src/main/java/electronicticketingsystem/view/TicketMachineUI.java
+++ src/main/java/electronicticketingsystem/view/TicketMachineUI.java
@@ -1,23 +1,47 @@
 package electronicticketingsystem.view;
 
 import java.util.*;
-
+import java.time.YearMonth;
 
 import electronicticketingsystem.controller.TicketMachine;
+import electronicticketingsystem.model.util.payment.CreditCard;
 import electronicticketingsystem.model.util.ticket.TicketCatalog;
 
+/**
+ * Classe che descrive la view del sistema. La classe gestisce l'interfaccia utente testuale dell'emettitrice, 
+ * limitandosi a effettuare alcune stampe e a richiamare gli opportuni metodi del controller TicketMachine quando
+ * sono richieste elaborazioni dei dati.
+ * Gli attributi sono:
+ * @param s				oggetto della classe Scanner usato per ottenere le informazioni inserite su console da 
+ * 						parte dell'utente
+ * @param choice 		valore intero che rappresenta la scelta del servizio effettuata dall'utente
+ * @param tm			oggetto della classe TicketMachine, ovvero della classe che fa da Controller del sistema
+ *
+ */
 public class TicketMachineUI {
 	
 	Scanner s;
 	int choice;
 	TicketMachine tm;
 	
+	/**
+	 * Costruttore della classe, che richiede in ingresso un oggetto della classe Scanner. Il costruttore inizializza 
+	 * l'attributo choice e crea un oggetto Controller.
+	 * @param s (Scanner)
+	 */
 	public TicketMachineUI(Scanner s) {
 		this.s=s;
 		choice=0;
 		tm=new TicketMachine();
 	}
 	
+	/**
+	 * Metodo che gestisce le operazioni effettuate dall'interfaccia grafica. Il metodo si limita ad effettuare una serie
+	 * di stampe di servizi selezionabili dall'utente e a richiamare i metodi opportuni del controller per permettere
+	 * all'utente di visualizzare a schermo i risultati.
+	 * @param cat		richiede in ingresso un oggetto della classe TicketCatalog perchè l'utente visualizzi i
+	 * 					prodotti acquistabili
+	 */
 	public void start(TicketCatalog cat) {
 		
 		do {
@@ -44,16 +68,33 @@ public class TicketMachineUI {
 						}
 					}while(type !=9);
 					System.out.println("The total is: "+ tm.getTotal());
-					System.out.println("Please, enter cash");
-					double cs=s.nextDouble();
-					tm.makePayment(cs);
-					System.out.println("The change is: \n" + tm.getChange());
-					tm.endSale();
-				
-				break;
+					System.out.println("Please, choose your payment method:\n1 - Cash\n2 - Credit Card");
+					int paymentMethod=s.nextInt();
+					switch(paymentMethod) {
+						case 1:
+							System.out.println("Please, enter cash");
+							double cs=s.nextDouble();
+							tm.makeCashPayment(cs);
+							System.out.println("The change is: \n" + tm.getChange());
+							tm.endSale();
+						break;
+						case 2:
+							System.out.println("Please submit your credit card details\nCard Number:");
+							String cardNumber = s.next();
+							System.out.println("Expiration Date:");
+							String exp = s.next();
+							YearMonth expDate = YearMonth.parse(exp);
+							System.out.println("CVV:");
+							String cvv = s.next();
+							CreditCard cc = new CreditCard(cardNumber, expDate, cvv);
+							tm.makeCreditCardPayment(cc);
+							tm.endSale();
+					};
+					
 					
 			};
 		}while(choice !=9);
 	}
+
 
 }
