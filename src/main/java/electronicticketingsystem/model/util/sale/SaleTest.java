15ff2cab63077c044f595fd6e85eb23243735026
stngatti <stinogatti.99@gmail.com>
2021-07-01 20:26:38 +0200

    Aggiunta classe di convalida JUnit SaleTest

---
+++ src/main/java/electronicticketingsystem/model/util/sale/SaleTest.java
@@ -0,0 +1,14 @@
+package electronicticketingsystem.model.util.sale;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SaleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
